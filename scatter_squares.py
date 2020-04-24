5ebd71d2dfa7c98d8cc5797b01c2f7a66a90e958
Rowan Shepherd <shepherd.rowan.k@gmail.com>
2020-04-23 22:04:01 -0700

    Create a file with individual plot points

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,12 +1,11 @@
 import matplotlib.pyplot as plt 
 
-# Corrects the default values
-input_values = [1,2,3,4,5]
-squares = [1,4,9,16,25]
+x_values = [1,2,3,4,5]
+y_values = [1,4,9,16,25]
 
-plt.style.use('seaborn-dark')
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.plot(input_values, squares, linewidth = 3)
+ax.scatter(x_values, y_values, s = 100)
 
 # Set chart title and label axes 
 ax.set_title("Square Numbers", fontsize = 24)
@@ -14,6 +13,6 @@ ax.set_xlabel("Value", fontsize = 14)
 ax.set_ylabel("Square of Value", fontsize = 14)
 
 # Set size of tick labels
-ax.tick_params(axis='both', labelsize = 14)
+ax.tick_params(axis='both', which ='major', labelsize =14)
 
 plt.show() 
